c109526990b9604a84f78c79763eb03756a298aa
GianlucaMinoprio <58565032+GianlucaMinoprio@users.noreply.github.com>
2023-03-18 14:05:54 +0100

    feat setup test

--- src/components/login_button.tsx
+++ src/components/login_button.tsx
@@ -1,8 +1,28 @@
 import React, { useCallback, useState } from 'react'
-import {getAddress, isConnected, getNFT } from "@gemwallet/api";
+import { getAddress, isConnected, getNFT, getPublicKey } from "@gemwallet/api";
 
-export default function loginButton() {
-    return ( <button onclick="connect()" class="waves-effect waves-light btn">
-    Login with GemWallet
-  </button>)
+export default function LoginButton() {
+  const [address, setAddress] = useState('');
+
+  const connect = useCallback(async () => {
+    const isConnected_ = await isConnected();
+    if (isConnected_) {
+      const responsePublicKey = await getPublicKey();
+      if (responsePublicKey) {
+        const { address: addr, publicKey } = responsePublicKey;
+        setAddress(addr);
+      }
+    } else {
+      alert(
+        "User doesn't have GemWallet! Please install it: https://gemwallet.app"
+      );
+    }
+  }, []);
+
+  return (
+    <>
+      <button onClick={connect}>Login with GemWallet</button>
+      <p id="address">{address}</p>
+    </>
+  );
 }
--- src/pages/test.tsx
+++ src/pages/test.tsx
@@ -1,10 +1,11 @@
-import loginButton from "@/components/login_button"
+import LoginButton from "@/components/login_button"
 
 export default function Test() {
     const lol = "lol"
   return (
     <div>
         <title>Test gem Wallet</title>
+        <LoginButton />
     </div>
   )
 }
